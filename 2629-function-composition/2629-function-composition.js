ff79f895428b2032bacddb99e024404b627d237d
Asiya Bint <163452363+killerand@users.noreply.github.com>
2024-05-29 03:27:12 +0900

    Time: 68 ms (32.65%), Space: 50.1 MB (40.58%) - LeetHub

--- 2629-function-composition/2629-function-composition.js
+++ 2629-function-composition/2629-function-composition.js
@@ -4,17 +4,7 @@
  */
 var compose = function(functions) {
     return function(x) {
-        if (functions.length === 0) {
-            // If the list of functions is empty, return the identity function
-            return x;
-        } else {
-            // Define the composed function recursively
-            let result = x;
-            for (let i = functions.length - 1; i >= 0; i--) {
-                result = functions[i](result);
-            }
-            return result;
-        }
+        // Apply each function from right to left
+        return functions.reduceRight((acc, fn) => fn(acc), x);
     }
 };
-
